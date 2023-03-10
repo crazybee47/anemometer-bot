70032ed321c36ad89e0dd254407bc77ad0b67916
user <dimochka1994@yandex.ru>
2023-03-10 11:51:00 +0300

    init 2

--- app/Core/Infrastructure/Service/Config.php
+++ app/Core/Infrastructure/Service/Config.php
@@ -5,7 +5,7 @@ namespace App\Core\Infrastructure\Service;
 class Config {
 
     public const TELEGRAM_BOT_USERNAME = 'WFkite_bot';
-    public const TELEGRAM_BOT_TOKEN    = '5527745968:AAGlmlU5sp6SgOG7ztET8cNZ7PERIo55QgQ';
-    public const TELEGRAM_BOT_HOOK_URL = 'https://advancedbot.ru/windfactory/hook.php';
+    public const TELEGRAM_BOT_TOKEN    = '';
+    public const TELEGRAM_BOT_HOOK_URL = '';
 
 }
